bc72b1c05108ed3c592928e998a7db9eb3fee3a3
Abhinav Kumar <abhinav03ks@gmail.com>
2025-03-05 09:42:50 +0000

    feat: Student Sign Up Page UI setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import LoginPage from "./components/LoginPage.jsx";
+// import LoginPage from "./components/LoginPage.jsx";
+import StudentSignup from "./components/signUpAsStudent.jsx";
 
 export default function App() {
   return (
     <>
-    <LoginPage/>
+      {/* <LoginPage></LoginPage> */}
+      <StudentSignup />
     </>
   );
 }
