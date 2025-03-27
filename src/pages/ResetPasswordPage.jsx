786dcf597a232ef5cb012c11d8194ad95fcb2a63
Swagatam <swagatamheroz@outlook.com>
2025-03-27 12:37:59 +0530

    chore: consolidate

--- src/pages/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage.jsx
@@ -1,32 +1,92 @@
+import { useParams } from "react-router-dom";
+
 import Navbar from "../components/Navbar.jsx";
 
 import styles from "../styles/reset-password-page.module.css";
 
 export default function ResetPasswordPage() {
+  const { category } = useParams();
+
   return (
     <div className={styles.container}>
       <Navbar />
 
       <main className={styles.content}>
         <div className={styles.card}>
-          <h2 className={styles.resetText}>Reset Password</h2>
+          <h2 className={styles.resetText}>
+            {category === "email-input" && "Reset Password"}
+            {category === "code-input" && "Check Your Inbox"}
+            {category === "password-input" && "Set New Password"}
+          </h2>
+
+          {category === "code-input" && (
+            <div className={styles.hint}>
+              A 6-digit reset code has been sent to the provided email address.
+            </div>
+          )}
+
+          {category === "password-input" && (
+            <div className={styles.hint}>
+              Create a new password. Ensure that it is different from the
+              previous password.
+            </div>
+          )}
 
           <form className={styles.form}>
             <label htmlFor="email" className={styles.label}>
-              Registered Email Address
+              {category === "email-input" && "Registered Email Address"}
+              {category === "code-input" && "Reset Code"}
             </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className={styles.input}
-              required
-            />
+
+            {category === "email-input" && (
+              <input
+                type="email"
+                name="email"
+                className={styles.input}
+                required
+              />
+            )}
+
+            {category === "code-input" && (
+              <input
+                type="number"
+                name="resetCode"
+                className={styles.input}
+                required
+              />
+            )}
+
+            {category === "password-input" && (
+              <>
+                <input
+                  type="password"
+                  name="password"
+                  className={styles.input}
+                  placeholder="New Password"
+                  required
+                />
+                <input
+                  type="password"
+                  name="password"
+                  className={styles.input}
+                  placeholder="Confirm Password"
+                  required
+                />
+              </>
+            )}
 
             <button type="submit" className={styles.btn}>
-              Get Reset Code
+              {category === "email-input" && "Get Reset Code"}
+              {category === "code-input" && "Verify Code"}
+              {category === "password-input" && "Update Password"}
             </button>
           </form>
+
+          {category === "code-input" && (
+            <p className={styles.sendAgain}>
+              Haven&apos;t received the code yet? <a href="">Resend mail</a>
+            </p>
+          )}
         </div>
       </main>
     </div>
