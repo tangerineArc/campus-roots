bcd7045eddcd69cddd039aecee1fc17e346c0095
Swagatam <swagatamheroz@outlook.com>
2025-04-01 23:08:06 +0530

    chore: enhance sidebar ui

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,12 @@
 import { Search } from "lucide-react";
+
 import styles from "../styles/search-bar.module.css";
 
 export default function SearchBar() {
   return (
-    <div className={styles.searchContainer}>
-      <input type="text" placeholder="Search..." className={styles.searchBar} />
-      <Search className={styles.searchIcon} />
-    </div>
+    <label className={styles.container}>
+      <input type="text" placeholder="search" className={styles.input} />
+      <Search className={styles.icon} />
+    </label>
   );
 }
