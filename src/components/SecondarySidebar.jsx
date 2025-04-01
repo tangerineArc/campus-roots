603a0f2022dfd2f53b688a0470ed6d7a8a5b58f7
Swagatam <swagatamheroz@outlook.com>
2025-04-02 02:34:31 +0530

    chore: add secondary sidebar

---
+++ src/components/SecondarySidebar.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/secondary-sidebar.module.css";
+
+export default function SecondarySidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      to-do
+    </aside>
+  );
+}
