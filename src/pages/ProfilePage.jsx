1ad039ed768581a8a434229905a910b53d092987
Swagatam <swagatamheroz@outlook.com>
2025-05-05 21:35:34 +0530

    fix: profile data fetching

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,63 @@
+import AchievementsSection from "../components/AchievementsSection.jsx";
+import EducationSection from "../components/EducationSection.jsx";
+import ExperienceSection from "../components/ExperienceSection.jsx";
+import ProfileSection from "../components/ProfileSection.jsx";
+import SideBar from "../components/Sidebar.jsx";
+import SkillsSection from "../components/SkillsSection.jsx";
+
+import { useAuth } from "../contexts/auth-context.jsx";
+
+import useFetch from "../hooks/use-fetch.js";
+
+import styles from "../styles/profile-page.module.css";
+
+const options = { credentials: "include" };
+
+export default function ProfilePage() {
+  const { user } = useAuth();
+
+  const {
+    data: profileData,
+    loading,
+    error,
+  } = useFetch(
+    `${import.meta.env.VITE_API_SERVER_URL}/user/${user.id}`,
+    options
+  );
+
+  if (loading) {
+    return <p>Fetching data...</p>;
+  } else {
+    console.log(profileData); // to be removed
+  }
+
+  if (error) {
+    return <p>Failed to fetch...</p>;
+  }
+
+  if (!profileData) {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <SideBar />
+
+      <main className={styles.main}>
+        <ProfileSection
+          currUserId={user.id}
+          data={{
+            id: profileData.user.id,
+            name: profileData.user.name,
+            about: profileData.user.about,
+            avatar: profileData.user.avatar,
+          }}
+        />
+        <ExperienceSection data={profileData} />
+        <EducationSection userProfileData={profileData} />
+        <SkillsSection userProfileData={profileData} />
+        <AchievementsSection userProfileData={profileData} />
+      </main>
+    </div>
+  );
+}
